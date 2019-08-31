8e2a9f1962b7dc4b4f477e7c04f3ac4a972710b6
{dp7jm} <{dpjm@chef.net}>
2019-08-31 22:03:21 +0200

    created activities for workflow engine

--- WorkflowCS/Program.cs
+++ WorkflowCS/Program.cs
@@ -8,6 +8,44 @@ namespace WorkflowCS
 {
     class Program
     {
+        public interface IActivity
+        {
+             void ExecuteA();
+        }
+
+        public class WorkflowEngine {
+            private List<IActivity> _activies;
+
+
+            public void Run()
+            {   
+
+
+
+            }
+        }
+        public class UploadCloudStorage : IActivity
+        {
+            public void ExecuteA()
+            {
+                Console.WriteLine("uploding image");
+
+            }
+        }
+        public class CallWebService : IActivity
+        {
+            public void ExecuteA()
+            {
+                Console.WriteLine("casll web service"); 
+            }
+        }
+        public class SendEmail : IActivity
+        {
+            public void ExecuteA()
+            {
+                Console.WriteLine("sednig e-mail your video is uploding"); ;
+            }
+        }
         static void Main(string[] args)
         {
         }
